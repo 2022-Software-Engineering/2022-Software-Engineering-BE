2acd9066c4e7430aa9c7e30fa6ec60b8f5dd38c9
Yujin_nKim <yujin.kimd@gmail.com>
2022-11-18 07:46:25 +0900

    feature: create User, InterestPlant table

--- PT/models.py
+++ PT/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    userID = models.TextField(primary_key=True)
+    userPW = models.TextField()
+    userName = models.TextField()
+
+class InterestPlant(models.Model):
+    userID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    plantID = models.TextField()
+    
+
