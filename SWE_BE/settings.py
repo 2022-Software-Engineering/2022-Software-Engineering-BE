a1efbf5b128d02b0c5c0f9ab5b382c374aedb134
Yujin_nKim <yujin.kimd@gmail.com>
2022-12-03 18:18:42 +0900

    fix settings.py

--- SWE_BE/settings.py
+++ SWE_BE/settings.py
@@ -62,28 +62,7 @@ CORS_ORIGIN_WHITELIST = [
     "https://2022-software-engineering.github.io",
 ]
 
-CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_METHODS = (
-   'DELETE',
-   'GET',
-   'OPTIONS',
-   'PATCH',
-   'POST',
-   'PUT',
-)
-CORS_ALLOW_HEADERS = (
-   'accept',
-   'accept-encoding',
-   'authorization',
-   'content-type',
-   'dnt',
-   'origin',
-   'user-agent',
-   'x-csrftoken',
-   'x-requested-with',
-)
-APPEND_SLASH = False
 
 ROOT_URLCONF = 'SWE_BE.urls'
 
